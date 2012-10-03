2fecd5c68ca96debf43399a017e5383d3dbec490
Sarven Capadisli <info@csarven.ca>
2012-10-03 18:06:40 +0200

    Creates datadumps

---
+++ scripts/create.tar.gz.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+libdata=/var/www/lib/transparency-linked-data/data
+data=/data/transparency-linked-data/data
+
+cp "$libdata"/country.nt "$data"/meta/
+cp "$libdata"/meta.ttl "$data"/meta/
+cp "$libdata"/transparency-sameAs-dbpedia_countries.nt "$data"/meta/
+cp "$libdata"/transparency-sameAs-eurostat_countries.nt "$data"/meta/
+cp "$libdata"/transparency-sameAs-geonames_countries.nt "$data"/meta/
+cp "$libdata"/transparency-sameAs-worldbank_countries.nt "$data"/meta/
+
+cp "$libdata"/CPI*.ttl "$data"/corruption-perceptions-index/
+
+cp "$libdata"/import/* "$data"/import/
+
+cd "$data"/meta
+tar -cvzf meta.tar.gz *
+mv "$data"/meta/meta.tar.gz "$data"/
+
+cd "$data"/corruption-perceptions-index
+tar -cvzf corruption-perceptions-index.tar.gz *
+mv "$data"/corruption-perceptions-index/corruption-perceptions-index.tar.gz "$data"/
