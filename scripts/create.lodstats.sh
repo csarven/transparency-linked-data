4dc9f074f628768a40d974f541e882ceae568169
Sarven Capadisli <info@csarven.ca>
2012-10-03 17:48:55 +0200

    LODStat script for meta and CPIs

---
+++ scripts/create.lodstats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. $HOME/lodstats-env/bin/activate
+
+libdataimport=/var/www/lib/transparency-linked-data/data/import
+cd "$libdataimport"
+lodstats -val "$libdataimport"/graph.meta.nt > "$libdataimport"/graph.meta.nt.stats.ttl
+echo "Created $libdataimport/graph.meta.nt.stats.ttl"
+lodstats -val "$libdataimport"/graph.transparency-international-corruption-perceptions-index.nt > "$libdataimport"/graph.transparency-international-corruption-perceptions-index.nt.stats.ttl
+echo "Created $libdataimport/graph.transparency-international-corruption-perceptions-index.nt.stats.ttl"
+
