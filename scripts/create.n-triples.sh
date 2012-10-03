c9d110f61bc694a188a4a74333d2581505e04f58
Sarven Capadisli <info@csarven.ca>
2012-10-03 17:22:24 +0200

    Creates N-Triples for your N-Triples needs

---
+++ scripts/create.n-triples.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rapper -gq /var/www/lib/transparency-linked-data/data/meta.ttl >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+
+cat /var/www/lib/transparency-linked-data/data/country.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+
+cat /var/www/lib/transparency-linked-data/data/transparency-sameAs-dbpedia_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+
+cat /var/www/lib/transparency-linked-data/data/transparency-sameAs-eurostat_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+
+#cat /var/www/lib/transparency-linked-data/data/transparency-sameAs-geonames_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+
+cat /var/www/lib/transparency-linked-data/data/transparency-sameAs-worldbank_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+
+for i in /var/www/lib/transparency-linked-data/data/CPI*.ttl; do rapper -gq "$i" >> /var/www/lib/transparency-linked-data/data/import/graph.cpi.nt; done;
