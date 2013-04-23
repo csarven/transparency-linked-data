4649d04380fb9eb7a689394ad37b231ed654a527
Sarven Capadisli <info@csarven.ca>
2013-04-23 15:06:33 +0200

    Minor

--- scripts/tdbloader.transparency.sh
+++ scripts/tdbloader.transparency.sh
@@ -4,11 +4,11 @@ data="/var/www/lib/transparency-linked-data/data/";
 namespace="http://transparency.270a.info/";
 db="/SSD/data/fuseki/DB/DB.transparency/";
 graphs="/home/sarcap/Graphs/";
+tdbAssembler="/usr/lib/fuseki/tdb2.transparency.ttl";
+JVM_ARGS="-Xmx16000M";
 
 echo "Removing $db";
 rm -rf "$db";
-
-tdbAssembler=/usr/lib/fuseki/tdb2.transparency.ttl;
 
 java tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"import/graph.meta.nt;
 java tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/corruption-perceptions-index "$data"import/graph.transparency-international-corruption-perceptions-index.nt;
