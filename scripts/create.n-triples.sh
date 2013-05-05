61a2e0e5b86c3cbb50a5f1b65cbb59cbc5e1253c
Sarven Capadisli <info@csarven.ca>
2013-05-05 19:06:35 +0200

    Command to add FAO interlinks

--- scripts/create.n-triples.sh
+++ scripts/create.n-triples.sh
@@ -14,4 +14,6 @@ cat /var/www/lib/transparency-linked-data/data/transparency-sameAs-geonames_coun
 
 cat /var/www/lib/transparency-linked-data/data/transparency-exactMatch-worldbank_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
 
+cat /var/www/lib/transparency-linked-data/data/transparency-exactMatch-fao_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+
 for i in /var/www/lib/transparency-linked-data/data/CPI*.ttl; do rapper -gq "$i" >> /var/www/lib/transparency-linked-data/data/import/graph.transparency-international-corruption-perceptions-index.nt; done;
