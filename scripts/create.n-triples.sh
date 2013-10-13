b407ff963b7c6dac6d0530cbd6703454d8d2a119
Sarven Capadisli <info@csarven.ca>
2013-10-13 14:39:03 +0200

    Updated interlinks

--- scripts/create.n-triples.sh
+++ scripts/create.n-triples.sh
@@ -12,14 +12,16 @@ cat /var/www/lib/transparency-linked-data/data/transparency-sameAs-eurostat_coun
 
 cat /var/www/lib/transparency-linked-data/data/transparency-sameAs-geonames_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
 
-cat /var/www/lib/transparency-linked-data/data/transparency-sameAs-iati_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+cat /var/www/lib/transparency-linked-data/data/transparency.exactMatch.iati.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
 
-cat /var/www/lib/transparency-linked-data/data/transparency-exactMatch-worldbank_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+cat /var/www/lib/transparency-linked-data/data/transparency.exactMatch.hr.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
 
-cat /var/www/lib/transparency-linked-data/data/transparency-exactMatch-fao_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+cat /var/www/lib/transparency-linked-data/data/transparency.exactMatch.worldbank.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
 
-cat /var/www/lib/transparency-linked-data/data/transparency-exactMatch-ecb_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+cat /var/www/lib/transparency-linked-data/data/transparency.exactMatch.fao.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
 
-cat /var/www/lib/transparency-linked-data/data/transparency-exactMatch-imf_countries.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+cat /var/www/lib/transparency-linked-data/data/transparency.exactMatch.ecb.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+
+cat /var/www/lib/transparency-linked-data/data/transparency.exactMatch.imf.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
 
 for i in /var/www/lib/transparency-linked-data/data/CPI*.ttl; do rapper -gq "$i" >> /var/www/lib/transparency-linked-data/data/import/graph.transparency-international-corruption-perceptions-index.nt; done;
