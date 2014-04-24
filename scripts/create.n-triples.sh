212286c09e0f1823925e41688e137889edaedc91
Sarven Capadisli <info@csarven.ca>
2014-04-24 16:22:43 +0200

    Minor

--- scripts/create.n-triples.sh
+++ scripts/create.n-triples.sh
@@ -24,4 +24,6 @@ cat /var/www/lib/transparency-linked-data/data/transparency.exactMatch.ecb.nt >>
 
 cat /var/www/lib/transparency-linked-data/data/transparency.exactMatch.imf.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
 
+cat /var/www/lib/transparency-linked-data/data/transparency.exactMatch.uis.nt >> /var/www/lib/transparency-linked-data/data/import/graph.meta.nt ;
+
 for i in /var/www/lib/transparency-linked-data/data/CPI*.ttl; do rapper -gq "$i" >> /var/www/lib/transparency-linked-data/data/import/graph.transparency-international-corruption-perceptions-index.nt; done;
