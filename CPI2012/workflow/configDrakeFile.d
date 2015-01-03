9d6a9146e3e2d8c8c806ba9cc13bc24b7b4bf00e
reni99 <renato.stauffer90@googlemail.com>
2015-01-03 02:19:22 +0100

    Delete unnecessary paths

--- CPI2012/workflow/configDrakeFile.d
+++ CPI2012/workflow/configDrakeFile.d
@@ -8,31 +8,25 @@ agencyURL="http://www.transparency.org/"
 agent="Renato Stauffer"
 data="../data";
 namespace="http://$agency.270a.info/";
+year=2012;
 
 ;Paths to description for workflows
 wfDesc="$[data]/wfDescription/";
 wfDescMainProcess="$[wfDesc]/$[agency].ttl";
 
-wfDescInspection="$[wfDesc]wfDescInspection/inspection-$[agency].ttl";
-wfDescExtraction="$[wfDesc]wfDescExtraction/extraction-$[agency].ttl";
-wfDescPreprocessing="$[wfDesc]wfDescPreprocessing/preprocessing-$[agency].ttl";
-wfDescMapping="$[wfDesc]wfDescMapping/mapping-$[agency].ttl";
-
-;Paths to workflow steps
-wfExtractionSteps="wfExtraction.d";
-wfInspectionSteps="wfInspection.d";
-wfPreprocessingSteps="wfPreprocessing.d";
-wfMappingSteps="wfMapping.d";
-wfCleanupSteps="wfCleanup.d";
-
 ;Paths to data
-CPI2012_ResultsXLS="$[data]/CPI2012_Results.xls";
 transparencyZIP="$[data]/transparency.zip";
-CPI2012_ResultsCSV="$[data]/CPI2012_Results.csv";
-countryCodesCSV="$[data]/countryCodes.csv";
-refinedCountryCodesCSV="$[data]/refinedCountryCodes.csv";
-refinedCPI2012CSV="$[data]/refinedCPI2012.csv";
-CPI2012csv="$[data]/CPI2012.csv";
-CPI2012_refinedCSV="$[data]/CPI2012_refined.csv";
-turtleTransparency="$[data]/turtleTransparency.ttl";
-turtleTransparency_final="$[data]/turtleTransparency_final.ttl";
+CPI_Results="$[data]/CPI.Results.csv";
+countryCodes="$[data]/country.codes.csv";
+sortedCountryCodes="$[data]/sorted.country.codes.csv";
+refinedCPIData="$[data]/refined.CPI.data.csv";
+mergedCPI="$[data]/merged.CPI.csv";
+refinedMergedCPI="$[data]/refined.merged.CPI.csv";
+rawTurtle="$[data]/raw.ttl";
+turtleTransparency="$[data]/transparency.ttl";
+
+;Path to worflow description files
+workflowTemplate="$[namespace]workflow/";
+workflowAccount="$[namespace]account/";
+workflowExecutionDescription="$[data]/workflowExecutionDescription.ttl";
+abstractWorkflowDescription="$[data]/abstractWorkflowDescription.ttl";
