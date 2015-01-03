5178f12e90aacafc8ca6c6c40212704d6e52dddc
reni99 <renato.stauffer90@googlemail.com>
2015-01-03 02:36:06 +0100

    Remove inspection workflow step.

--- CPI2012/workflow/wfInspection.d
+++
@@ -1,9 +0,0 @@
-%include configDrakeFile.d
-
-;Inspect data
-%1StepInspection <-
- date=`date +%Y-%m-%d:%H:%M:%S`
- echo "<$[namespace]workflow/$[agency]/inspection/1StepInspection> a opmv:Process, prov:Activity, opmw:WorkflowExecutionProcess;" >> "$[wfDescInspection]"
- echo "opmw:correspondsToTemplateProcess <$[namespace]workflow/$[agency]/inspection>." >> "$[wfDescInspection]"
- echo "Inspected data from $[agencyLabel]"
- echo "Workflow step was executed by $[agent] at $date"
